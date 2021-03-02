04fc2889543f4fc2d0af74847c344ff57410c0da
azusa5526 <azusa5526@gmail.com>
2021-03-03 04:29:54 +0800

    add anime & puipui sound

--- src/App.vue
+++ src/App.vue
@@ -4,17 +4,25 @@
     <div class="container main-container">
       <div class="row">
         <div class="col-12">
-          <div class="bg-third text-center py-9 py-lg-11 my-3 my-lg-6">
-            <h1>アニメーション PUI PUI モルカー</h1>
-            <p class="lead">Infinite Scroll</p>
+          <div class="bg-third text-center intro py-8 py-lg-10 my-3 my-lg-6">
+            <h1 class="fs-2">PUI PUI モルカー</h1>
+            <p class="mb-0">Infinite Scroll</p>
           </div>
         </div>
       </div>
 
       <div class="row">
         <div class="col-12 col-lg-8 order-1 order-lg-0">
-          <div class="card bg-secondary mb-6" v-for="repo in repoData" :key="repo.id">
-            <div class="card-header bg-third text-muted">main lang > {{ repo.language }}</div>
+          <div
+            class="card bg-secondary wow animate__animated animate__fadeInLeft mb-6"
+            data-wow-duration=".5s"
+            v-for="repo in repoData"
+            :key="repo.id"
+          >
+            <div v-if="repo.language" class="card-header bg-third text-muted">
+              {{ repo.language }}
+            </div>
+            <div v-else class="card-header bg-third text-muted">none primary language</div>
             <div class="card-body">
               <h3 class="card-title">{{ repo.name }}</h3>
               <p v-if="repo.description === null" class="card-text fw-light mb-8">
@@ -23,9 +31,11 @@
               <p v-else class="card-text fw-light mb-8">
                 {{ repo.description }}
               </p>
-              <a :href="repo.html_url" class="btn btn-primary"
-                >Repo Link<i class="fas fa-link text-dark ms-3"></i
-              ></a>
+              <div class="d-flex justify-content-end">
+                <a :href="repo.html_url" class="btn btn-primary"
+                  >Repo Link<i class="fas fa-link text-dark ms-3"></i
+                ></a>
+              </div>
             </div>
             <div
               class="card-footer bg-third text-end text-muted border-2 border-top border-primary"
@@ -40,8 +50,17 @@
           <div class="sidebar-item">
             <div class="sticky-section">
               <div class="card bg-secondary mb-6">
+                <div class="card-header bg-third">
+                  <span class="text-muted">Pet me plz</span>
+                </div>
                 <div class="card-body">
-                  <img class="w-100 mb-6" src="./assets/img/avatar.jpg" alt="" />
+                  <img
+                    class="avatar w-100 wow animate__animated animate__pulse mb-6"
+                    data-wow-duration="1s"
+                    data-wow-delay="1s"
+                    src="./assets/img/avatar.jpg"
+                    alt=""
+                  />
                   <h3 class="card-title"><i class="fab fa-github-alt fs-4 me-3"></i>azusa5526</h3>
                   <p class="card-text">Hi! My name is Andrew. This is my github page.</p>
                   <div class="d-flex justify-content-end mt-11">
@@ -74,6 +93,8 @@
         </div>
       </div>
     </div>
+
+    <audio id="audio" preload="auto" src="./assets/audio/puipui.mp3"></audio>
   </div>
 </template>
 
@@ -147,6 +168,16 @@ export default {
     };
     const observer = new IntersectionObserver(this.handleIntersection, options);
     observer.observe(this.$refs.target);
+
+    const avatar = document.querySelector('.avatar');
+    avatar.addEventListener(
+      'click',
+      function (e) {
+        const audio = document.getElementById('audio');
+        audio.play();
+      },
+      false
+    );
   }
 };
 </script>
--- src/main.js
+++ src/main.js
@@ -1,10 +1,13 @@
 import Vue from 'vue';
 import App from './App.vue';
 
+import 'bootstrap/dist/js/bootstrap.bundle.js';
 import axois from 'axios';
 import VueAxios from 'vue-axios';
 import Loading from 'vue-loading-overlay';
 import 'vue-loading-overlay/dist/vue-loading.css';
+import animated from 'animate.css';
+import WOW from 'wow.js';
 import isoTimeConvert from './filters/isoTimeConvert.js';
 
 Vue.config.productionTip = false;
@@ -14,6 +17,17 @@ Vue.use(VueAxios, axois);
 
 // Global Loading component
 Vue.component('Loading', Loading);
+
+// animated.css & wow.js
+Vue.use(animated);
+new WOW({
+  boxClass: 'wow',
+  animateClass: 'animated',
+  offset: 75,
+  mobile: true,
+  live: true,
+  resetAnimation: true
+}).init();
 
 // Filter
 Vue.filter('isoTimeConvert', isoTimeConvert);
